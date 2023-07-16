7c8beb23ec0a859fc4ab2ca6a1af03598ad82cf2
Forbes85 <vudo8585@gmail.com>
2023-07-16 09:04:55 -0500

    changed files and updated demo video link

--- models/Tag.js
+++ models/Tag.js
@@ -6,16 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    id: {
+    id:{
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
-    tag_name: {
-      type: DataTypes.STRING,
-    },
-    // define columns
+    tag_name:{
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
